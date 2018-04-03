d7bb29d43852e65adb04530a24084f2bf0b33e8f
u211-15 <u211-15@public.mpei.local>
2018-04-03 12:56:37 +0300

    3

---
+++ Temperature.h
@@ -0,0 +1,19 @@
+//
+// Created by u211-15 on 03.04.2018.
+//
+
+#ifndef UNTITLED_TEMPERATURE_H
+#define UNTITLED_TEMPERATURE_H
+#include <iostream>
+#include <sstream>
+using namespace std;
+struct Temperature{
+    double temp;
+    char scale;
+};
+istream & operator >> (istream&in, Temperature&Temp);
+double convert (const Temperature& Temp, char scale_to);
+bool operator < (const Temperature& lhs, const Temperature& rhs);
+Temperature operator- (const Temperature& lhs, const Temperature& rhs);
+Temperature operator/ (const Temperature& lhs, const Temperature& rhs);
+#endif //UNTITLED_TEMPERATURE_H
